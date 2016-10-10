c4e39c4973797dec70ce3b196e3005379ab076d3
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-10-10 18:36:54 +0200

    :art: about

--- src/js/animYearAbout.js
+++ src/js/animYearAbout.js
@@ -5,10 +5,45 @@ window.requestAnimFrame = require('./requestAnimFrame.js');
 // var detectScrollDir = require('./detectScrollDir.js');
 
 module.exports = function(){
-    var yearWrapper = $('#year'), years = $('[data-year]'), yearsHtml = '';
+    var yearWrapper = $('#year'), yearsData = $('[data-year]'), yearsHtml = '', style = '',
+        years, initialTop, thisYear, thisYearPos;
 
-    years.each(function(){
-        yearsHtml += '<div style="top: '+ $(this).offset().top +'px">' + $(this).data('year') + '</div>';
+    yearsData.each(function(i){
+        if(i === 0){
+            initialTop = ($(this).offset().top - 55) | 0;
+            style = 'top:' + initialTop +'px';
+        }else if(i === 1){
+            style += ';opacity:0';
+        }
+        yearsHtml += '<div class="year" data-top="'+ (($(this).offset().top - 55) | 0) +'" style="'+ style +'">' + $(this).data('year') + '</div>';
     });
     yearWrapper.append(yearsHtml);
+
+    years = yearWrapper.find('.year');
+
+    $(document).on('scroll', function(){
+        myScroll = $(this).scrollTop();
+
+        years.each(function(){
+            /*if($(this).data('y') < $(this).data('top')){
+                $(this).data('y', initialTop + myScroll);
+                TweenMax.set($(this), {y: $(this).data('y') + 'px'});
+            }else if(!$(this).hasClass('fixed')){
+                $(this).addClass('fixed');
+            }*/
+
+            thisYear = $(this);
+            thisYearPos = thisYear.data('top');
+
+            if(thisYear.hasClass('fixed')){
+                if(myScroll + initialTop < thisYearPos){
+                    thisYear.css('top', initialTop + 'px').removeClass('fixed');
+                }
+            }else{
+                if(thisYear.offset().top >= thisYearPos){
+                    thisYear.css('top', thisYearPos + 'px').addClass('fixed');
+                }
+            }
+        });
+    });
 }
