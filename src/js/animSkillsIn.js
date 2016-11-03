b7040d518d04358c78f7ab8ab31c69a1710c86a5
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-11-03 10:54:53 +0100

    :art: remove unused variables

--- src/js/animSkillsIn.js
+++ src/js/animSkillsIn.js
@@ -2,17 +2,15 @@ var $ = require('./libs/jquery/dist/jquery.min.js');
 var createTl = require('./tlSkillsIn.js');
 
 module.exports = function(){
-    var dashes = $('.dashes >span'), tlInDashes,
-        wrapperWaves = $('.wrapper-waves'), waves = $('.waves'), tlInWaves,
-        wrapperZigzags = $('.wrapper-zigzags'), zigzags = $('.zigzags'), tlInZigzags,
-        slashes = $('.slashes >span'), tlInSlashes,
-        dots = $('.dots >span'), tlInDots,
-        dataSkill, symbolToAnimate,
+    var dashes = $('.dashes').find('> span'),
+        wrapperWaves = $('.wrapper-waves'), waves = $('.waves'),
+        wrapperZigzags = $('.wrapper-zigzags'), zigzags = $('.zigzags'),
+        slashes = $('.slashes').find('>span'),
+        dots = $('.dots').find('>span'),
         tableTl = [];
 
     if(dashes.length){
         tableTl.push(createTl(true, dashes, true).play());
-        
     }
     if(wrapperWaves.length){
         tableTl.push(createTl(false, waves, wrapperWaves).play());
