5c2716f53b155a24a9a965a61d8321d65fd8e4c2
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-11-03 11:17:15 +0100

    :art: remove unused jquery

--- src/js/sinusoid.js
+++ src/js/sinusoid.js
@@ -1,6 +1,4 @@
-var $ = require('./libs/jquery/dist/jquery.min.js');
-
-module.exports = function(sinDivide, x, top, ampl) {
+module.exports = function(sinDivide, x, top, ampl){
 	var xReturn = x + ampl * Math.sin(top / sinDivide);
     return xReturn;
 }
