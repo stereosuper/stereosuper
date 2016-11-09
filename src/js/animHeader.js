625c6bb8fc9b470d0b5c74f172950da35098e5ee
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-11-09 17:13:23 +0100

    :art: improve header animation perf

--- src/js/animHeader.js
+++ src/js/animHeader.js
@@ -1,38 +1,38 @@
 var $ = require('./libs/jquery/dist/jquery.slim.min.js');
 var isMobile = require('./isMobile.min.js');
 
-window.requestAnimFrame = require('./requestAnimFrame.js');
 var detectScrollDir = require('./detectScrollDir.js');
+var throttle = require('./throttle.js');
+window.requestAnimFrame = require('./requestAnimFrame.js');
 
 module.exports = function(myScroll, body, header, skillsHome){
     var lastScroll = myScroll, scrollDir = 0;
 
     function scrollHeader(){
         myScroll = $(document).scrollTop();
+        scrollDir = detectScrollDir(myScroll, lastScroll);
 
-        if(myScroll !== lastScroll){
-            scrollDir = detectScrollDir(myScroll, lastScroll);
+        skillsHome = $('#skillsHome');
 
-            skillsHome = $('#skillsHome');
-
-            // synch with animSkillsScroll.js (only on home)
-            if(!body.hasClass('home') || (header.hasClass('scrolled') && myScroll > skillsHome.data('top') + 200 && skillsHome.is(':visible'))){
-                if(scrollDir > 0){
-                    header.removeClass('off');
-                }else if(scrollDir < 0){
-                    myScroll < 20 ? header.removeClass('off') : header.addClass('off');
-                }
-            }else{
+        // synch with animSkillsScroll.js (only on home)
+        if(!body.hasClass('home') || (header.hasClass('scrolled') && myScroll > skillsHome.data('top') + 200 && skillsHome.is(':visible'))){
+            if(scrollDir > 0){
                 header.removeClass('off');
+            }else if(scrollDir < 0){
+                myScroll < 20 ? header.removeClass('off') : header.addClass('off');
             }
+        }else{
+            header.removeClass('off');
         }
 
         lastScroll = myScroll;
-
-        requestAnimFrame(scrollHeader);
     }
 
+    var scrollHandler = throttle(function(){
+        requestAnimFrame(scrollHeader);
+    }, 40);
+
     if(!isMobile.any){
-        scrollHeader();
+        $(document).on('scroll', scrollHandler);
     }
 }
