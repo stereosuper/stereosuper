64c0d6cef25823789616991ae6eef776df386bc8
adrienlementheour <adrien.le.mentheour@gmail.com>
2016-11-15 11:21:51 +0100

    :art: Video background 1 attribute

--- src/js/animTop.js
+++ src/js/animTop.js
@@ -11,12 +11,16 @@ module.exports = function(myScroll, body, header){
     function createVideo(){
         var options = {
             id: 189647536,
+            byline: false,
             autoplay: true,
-            byline: false, 
             loop: true,
             title: false
         };
         player = new Vimeo.Player('video', options);
+        player.ready().then(function(){
+            var src = $('#video iframe').attr('src');
+            $('#video iframe').attr('src', src + "&background=1");
+        });
     }
 
     function onScroll(){
