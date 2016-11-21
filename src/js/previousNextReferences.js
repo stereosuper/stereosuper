74fe5b417f2c9cba06708485e994f7e31e34d722
Elisabeth Hamel <elisabeth@stereosuper.fr>
2016-11-21 18:15:30 +0100

    :art: improve nav single

--- src/js/previousNextReferences.js
+++ src/js/previousNextReferences.js
@@ -1,31 +1,24 @@
 var $ = require('./libs/jquery/dist/jquery.slim.min.js');
 var TweenMax = require('./libs/gsap/src/uncompressed/TweenMax.js');
-var TimelineMax = require('./libs/gsap/src/uncompressed/TimelineMax.js');
 
 module.exports = function(){
-	var previousNextButton = $('.previous-next-references >li a'),
-		previousButton = $('.previous-reference .txt'),
-		nextButton = $('.next-reference .txt'),
-		previousButtonSpan = previousButton.find('>span');
-		nextButtonSpan = nextButton.find('>span'),
-		previousArrow = $('.previous-reference .icon'),
-		nextArrow = $('.next-reference .icon'),
-		easeButton = Power3.easeOut,
-		tpsAnimIn = 0.2,
-		tpsAnimInTxt = 0.3,
-		tpsAnimOut = 0.3;
+	var navSingle = $('#navSingle'),
+		previousButtonSpan = navSingle.find('.previous-reference').find('.txt').find('>span');
+		nextButtonSpan = navSingle.find('.next-reference').find('.txt').find('>span'),
+		previousArrow = navSingle.find('.previous-reference').find('.icon'),
+		nextArrow = navSingle.find('.next-reference').find('.icon'),
+		easeButton = Power3.easeOut, tpsAnimIn = 0.2, tpsAnimInTxt = 0.3, tpsAnimOut = 0.3;
 
-	previousNextButton.on('mouseenter', function(){
+	navSingle.on('mouseenter focusin', 'a', function(){
+		TweenMax.to($(this).find('.txt').find('> span'), tpsAnimInTxt, {x: 0, opacity: 1, ease: easeButton});
 		if($(this).parents('li').hasClass('previous-reference')){
-			TweenMax.to(previousButtonSpan, tpsAnimInTxt, {x: 0, opacity: 1, ease: easeButton});
 			TweenMax.to(previousArrow, tpsAnimIn, {x: -10, ease: easeButton});
 			TweenMax.to(nextArrow, tpsAnimIn, {opacity: 0.5, ease: easeButton});
 		}else if($(this).parents('li').hasClass('next-reference')){
-			TweenMax.to(nextButtonSpan, tpsAnimInTxt, {x: 0, opacity: 1, ease: easeButton});
 			TweenMax.to(nextArrow, tpsAnimIn, {x: 10, ease: easeButton});
 			TweenMax.to(previousArrow, tpsAnimIn, {opacity: 0.5, ease: easeButton});
 		}
-	}).on('mouseleave', function(){
+	}).on('mouseleave focusout', 'a', function(){
 		TweenMax.to(previousButtonSpan, tpsAnimOut, {x: 20, opacity: 0, ease: easeButton});
 		TweenMax.to(nextButtonSpan, tpsAnimOut, {x: -20, opacity: 0, ease: easeButton});
 		TweenMax.to([previousArrow, nextArrow], tpsAnimOut, {x: 0, opacity: 1, ease: easeButton});
