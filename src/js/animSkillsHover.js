2cbae7df523acb1de32e74dd415708e2e15128fd
adrienlementheour <adrien.le.mentheour@gmail.com>
2016-11-22 14:53:06 +0100

    :art: Filter portfolio items

--- src/js/animSkillsHover.js
+++ src/js/animSkillsHover.js
@@ -14,7 +14,8 @@ module.exports = function(body, portfolioItems){
         wrapperZigzags = $('.wrapper-zigzags'), zigzags = $('.zigzags'), tlHoverZigzags,
         slashes = $('.slashes >span'), tlHoverSlashes,
         dots = $('.dots >span'), tlHoverDots,
-        dataSkill, symbolToAnimate;
+        dataSkill, symbolToAnimate,
+        portfolioItemOpacity;
 
     function switchSkills(data){
         switch(data){
@@ -52,60 +53,12 @@ module.exports = function(body, portfolioItems){
         tlHoverDots = createTlHover(dots.closest('.symbol').find('.hoverAnimation'), -35);
     }
 
+
+    
     body.on('mouseenter', '.skill', function(){
         if($(this).parents('body').hasClass('home')){
-            ///////////////////////////////////////
-            // masquer les projets déjà selected //
-            if(parentSkills.hasClass('selected')){
-                skills.removeClass('off');
-                portfolioItems.removeClass('off');
-                TweenMax.to([portfolioItems.find('.wrapper-bg-img'), portfolioItems.find('.bg')], 0.2, {opacity: 1});
-            }
-            ///////////////////////////////////////
-
             thisData = $(this).data('skill');
             switchSkills(thisData);
-
-            portfolioItems.each(function(){
-                theseDatas = stringToArray($(this).data('skill'));
-                if(theseDatas.indexOf(thisData) < 0){
-                    $(this).addClass('off');
-                    TweenMax.to([$(this).find('.wrapper-bg-img'), $(this).find('.bg')], 0.2, {opacity: 0});
-                }
-            });
-        }else{
-            skills.removeClass('off');
-            $('.skills.selected').removeClass('selected');
-            parentSkills.data('skill-selected', '');
-        }
-    }).on('mouseleave', '.skill', function(){
-        if($(this).parents('body').hasClass('home')){
-            if(parentSkills.hasClass('selected')){
-                thisData = $(this).data('skill');
-                parentSkillsData = parentSkills.data('skill-selected');
-                if(thisData != parentSkillsData){
-                    // On ferme le courant
-                    portfolioItems.removeClass('off');
-                    symbolToAnimate = $(this).find('.hoverAnimation');
-                    TweenMax.to(symbolToAnimate, 0.3, {scaleX: 1, x: 0});
-                    TweenMax.to([portfolioItems.find('.wrapper-bg-img'), portfolioItems.find('.bg')], 0.2, {opacity: 1});
-
-                    // On remet l'ancien
-                    $('.skill[data-skill="'+parentSkillsData+'"]').siblings().removeClass('off').addClass('off');
-                    portfolioItems.each(function(){
-                        theseDatas = stringToArray($(this).data('skill'));
-                        if(theseDatas.indexOf(parentSkillsData) < 0){
-                            $(this).addClass('off');
-                            TweenMax.to([$(this).find('.wrapper-bg-img'), $(this).find('.bg')], 0.2, {opacity: 0});
-                        }
-                    });
-                }
-            }else{
-                portfolioItems.removeClass('off');
-                symbolToAnimate = $(this).find('.hoverAnimation');
-                TweenMax.to(symbolToAnimate, 0.3, {scaleX: 1, x: 0});
-                TweenMax.to([portfolioItems.find('.wrapper-bg-img'), portfolioItems.find('.bg')], 0.2, {opacity: 1});
-            }
         }else{
             skills.removeClass('off');
             $('.skills.selected').removeClass('selected');
@@ -116,13 +69,90 @@ module.exports = function(body, portfolioItems){
             thisData = $(this).data('skill');
             parentSkillsData = parentSkills.data('skill-selected');
             switchSkills(thisData);
-            if(parentSkills.hasClass('selected') && (thisData === parentSkillsData)){
-                parentSkills.removeClass('selected');
-                parentSkills.data('skill-selected', '');
-                skills.removeClass('off');
+            if(parentSkills.hasClass('selected')){
+                if(thisData === parentSkillsData){
+                    parentSkills.removeClass('selected');
+                    parentSkills.data('skill-selected', '');
+                    skills.removeClass('off');
+                    //
+                    portfolioItems.removeClass('off');
+                    symbolToAnimate = $(this).find('.hoverAnimation');
+                    // TweenMax.to(symbolToAnimate, 0.3, {scaleX: 1, x: 0});
+                    TweenMax.to([portfolioItems.find('.wrapper-bg-img'), portfolioItems.find('.bg')], 0.2, {opacity: 1});
+                    TweenMax.set(portfolioItems, {opacity: 1});
+                    TweenMax.to([portfolioItems.find('time'), portfolioItems.find('.title')], 0.2, {x: 0, opacity: 1});
+                    TweenMax.to(portfolioItems.find('.border-left'), 0.2, {scaleY: 1});
+                    TweenMax.to(portfolioItems.find('.border-middle'), 0.2, {scaleX: 1});
+                }else{
+                    $(this).removeClass('off');
+                    $(this).siblings().addClass('off');
+                    parentSkills.data('skill-selected', thisData);
+
+                    portfolioItems.removeClass('off');
+
+                    // Dans un 1er temps, tous les items sont inactives
+                    portfolioItems.each(function(){
+                        $(this).addClass('off');
+                        TweenMax.to([$(this).find('.wrapper-bg-img'), $(this).find('.bg')], 0.2, {opacity: 0});
+                        portfolioItemOpacity = $(this).data('opacity');
+                        TweenMax.set($(this), {opacity: portfolioItemOpacity});
+
+                        // Disparition de l'année et titre du projet
+                        TweenMax.to([$(this).find('time'), $(this).find('.title')], 0.2, {x: 20, opacity: 0});
+                        TweenMax.to($(this).find('.border-left'), 0.2, {scaleY: 0});
+                        TweenMax.to($(this).find('.border-middle'), 0.2, {scaleX: 0});
+                    });
+                    setTimeout(function(){
+                        // Ensuite on ré-active les items concernés
+                        portfolioItems.each(function(){
+                            theseDatas = stringToArray($(this).data('skill'));
+                            if(theseDatas.indexOf(thisData) >= 0){
+                                $(this).removeClass('off');
+                                TweenMax.to([$(this).find('.wrapper-bg-img'), $(this).find('.bg')], 0.2, {opacity: 1});
+                                TweenMax.set($(this), {opacity: 1});
+
+                                // Réapparition de l'année et titre du projet
+                                TweenMax.to([$(this).find('time'), $(this).find('.title')], 0.2, {x: 0, opacity: 1});
+                                TweenMax.to($(this).find('.border-left'), 0.2, {scaleY: 1});
+                                TweenMax.to($(this).find('.border-middle'), 0.2, {scaleX: 1});
+                            }
+                        });
+                    }, 200);
+                }
             }else{
                 $(this).siblings().addClass('off');
                 parentSkills.addClass('selected').data('skill-selected', thisData);
+
+                portfolioItems.removeClass('off');
+
+                // Dans un 1er temps, tous les items sont inactives
+                portfolioItems.each(function(){
+                    $(this).addClass('off');
+                    TweenMax.to([$(this).find('.wrapper-bg-img'), $(this).find('.bg')], 0.2, {opacity: 0});
+                    portfolioItemOpacity = $(this).data('opacity');
+                    TweenMax.set($(this), {opacity: portfolioItemOpacity});
+
+                    // Disparition de l'année et titre du projet
+                    TweenMax.to([$(this).find('time'), $(this).find('.title')], 0.2, {x: 20, opacity: 0});
+                    TweenMax.to($(this).find('.border-left'), 0.2, {scaleY: 0});
+                    TweenMax.to($(this).find('.border-middle'), 0.2, {scaleX: 0});
+                });
+                setTimeout(function(){
+                    // Ensuite on ré-active les items concernés
+                    portfolioItems.each(function(){
+                        theseDatas = stringToArray($(this).data('skill'));
+                        if(theseDatas.indexOf(thisData) >= 0){
+                            $(this).removeClass('off');
+                            TweenMax.to([$(this).find('.wrapper-bg-img'), $(this).find('.bg')], 0.2, {opacity: 1});
+                            TweenMax.set($(this), {opacity: 1});
+
+                            // Réapparition de l'année et titre du projet
+                            TweenMax.to([$(this).find('time'), $(this).find('.title')], 0.2, {x: 0, opacity: 1});
+                            TweenMax.to($(this).find('.border-left'), 0.2, {scaleY: 1});
+                            TweenMax.to($(this).find('.border-middle'), 0.2, {scaleX: 1});
+                        }
+                    });
+                }, 200);
             }
         }else{
             skills.removeClass('off');
@@ -133,15 +163,17 @@ module.exports = function(body, portfolioItems){
 
     body.on('mouseenter', '.portfolio-item', function(){
         if($(this).parents('body').hasClass('home')){
-            $(this).removeClass('off');
-            skills.removeClass('off');
-            TweenMax.to([$(this).find('.wrapper-bg-img'), $(this).find('.bg')], 0.2, {opacity: 1});
+            if(!$(this).hasClass('off')){
+                $(this).removeClass('off');
+                skills.removeClass('off');
+                TweenMax.to([$(this).find('.wrapper-bg-img'), $(this).find('.bg')], 0.2, {opacity: 1});
 
-            theseDatas = stringToArray($(this).data('skill'));
-            skills.each(function(){
-                thisData = $(this).data('skill');
-                theseDatas.indexOf(thisData) < 0 ? $(this).addClass('off') : switchSkills(thisData);
-            });
+                theseDatas = stringToArray($(this).data('skill'));
+                skills.each(function(){
+                    thisData = $(this).data('skill');
+                    theseDatas.indexOf(thisData) < 0 ? $(this).addClass('off') : switchSkills(thisData);
+                });
+            }
         }else{
             skills.removeClass('off');
             $('.skills.selected').removeClass('selected');
@@ -149,21 +181,23 @@ module.exports = function(body, portfolioItems){
         }
     }).on('mouseleave', '.portfolio-item', function(){
         if($(this).parents('body').hasClass('home')){
-            skills.removeClass('off');
-            symbolToAnimate = $('.skill').find('.hoverAnimation');
-            TweenMax.set(symbolToAnimate, {scaleX: 1, x: 0});
+            if(!$(this).hasClass('off')){
+                skills.removeClass('off');
+                symbolToAnimate = $('.skill').find('.hoverAnimation');
+                TweenMax.set(symbolToAnimate, {scaleX: 1, x: 0});
 
-            if(parentSkills.hasClass('selected')){
-                // On remet l'ancien
-                parentSkillsData = parentSkills.data('skill-selected');
-                $('.skill[data-skill="'+parentSkillsData+'"]').siblings().addClass('off');
-                portfolioItems.each(function(){
-                    theseDatas = stringToArray($(this).data('skill'));
-                    if(theseDatas.indexOf(parentSkillsData) < 0){
-                        $(this).addClass('off');
-                        TweenMax.to([$(this).find('.wrapper-bg-img'), $(this).find('.bg')], 0.2, {opacity: 0});
-                    }
-                });
+                if(parentSkills.hasClass('selected')){
+                    // On remet l'ancien
+                    parentSkillsData = parentSkills.data('skill-selected');
+                    $('.skill[data-skill="'+parentSkillsData+'"]').siblings().addClass('off');
+                    portfolioItems.each(function(){
+                        theseDatas = stringToArray($(this).data('skill'));
+                        if(theseDatas.indexOf(parentSkillsData) < 0){
+                            $(this).addClass('off');
+                            TweenMax.to([$(this).find('.wrapper-bg-img'), $(this).find('.bg')], 0.2, {opacity: 0});
+                        }
+                    });
+                }
             }
         }else{
             skills.removeClass('off');
@@ -172,3 +206,4 @@ module.exports = function(body, portfolioItems){
         }
     });
 }
+
