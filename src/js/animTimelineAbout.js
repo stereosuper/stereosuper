29bb40197d757ee72a7010733790a9653f84a1a1
adrienlementheour <adrien.le.mentheour@gmail.com>
2016-11-23 15:35:16 +0100

    :bug: Timeline overflow hidden

--- src/js/animTimelineAbout.js
+++ src/js/animTimelineAbout.js
@@ -5,7 +5,7 @@ var checkScrollSpeed = require('./checkScrollSpeed.js');
 window.requestAnimFrame = require('./requestAnimFrame.js');
 
 module.exports = function(body){
-	var years = $('#year').find('.year'), timeline = $('#timeline'), contentTimeline = $('#content-timeline'), tl = $('#tl'),
+	var years = $('#year').find('.year'), timeline = $('#timeline'), contentTimeline = $('#content-timeline'), tl = $('#tl'), containerTimeline = $('.container-timeline'), timelineBottom,
 		rCheck, scaleValue;
 
     var firstYearTop = years.eq(0).data('top');
@@ -13,22 +13,34 @@ module.exports = function(body){
     var containerTimelineHeight = contentTimeline.outerHeight();
     var posiDownScroll = -containerTimelineHeight + centerFirstYearTop;
     var posiUpScroll = centerFirstYearTop;
+    var myScroll, tlTop;
 
     (function onScroll(){
         if(!body.hasClass('about')) return;
 
         rCheck = checkScrollSpeed();
         scaleValue = Math.abs(rCheck[0]/100)*3;
+
+        myScroll = $(document).scrollTop();
+        timelineBottom = containerTimeline.outerHeight() - 15;
+        TweenMax.set(contentTimeline, {opacity: 1});
         if(rCheck[1] === true){
         	// downscroll
         	TweenMax.set(contentTimeline, {y: posiDownScroll+'px'});
             TweenMax.set(tl, {transformOrigin: '0 100%'});
+            tlTop = contentTimeline.outerHeight()+myScroll+posiDownScroll;
         }else{
     		// upscroll
     		TweenMax.set(contentTimeline, {y: posiUpScroll+'px'});
             TweenMax.set(tl, {transformOrigin: '0 0'});
+            tlTop = contentTimeline.offset().top;
         }
         TweenMax.to(tl, 1, {scaleY: scaleValue});
+        
+        console.log('tlTop : '+tlTop+' / timelineBottom + 20 : '+(timelineBottom+20));
+        if(tlTop >= timelineBottom + 20){
+            TweenMax.set(contentTimeline, {opacity: 0});
+        }
 
         requestAnimFrame(onScroll);
     })();
