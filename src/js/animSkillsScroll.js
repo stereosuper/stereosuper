26301dd40f3a9c2b3cc4faeae7bac068b71b4630
Boris Forconi <boris@stereosuper.fr>
2016-11-26 00:54:15 +0100

    :bug: + fix fixed skillz #21

--- src/js/animSkillsScroll.js
+++ src/js/animSkillsScroll.js
@@ -20,7 +20,7 @@ module.exports = function(myScroll, body, header, skillsHome){
         // synch with animHeader.js
         if(header.hasClass('scrolled') && myScroll > skillsHome.data('top') && skillsHome.is(':visible')){
             skillsHome.addClass('fixed').removeClass('top');
-            if(scrollDir <= 0 && header.hasClass('off')){
+            if(scrollDir <= 0 && header.hasClass('off') || myScroll >= $(document).height() - $(window).height()){
                 skillsHome.addClass('top').addClass('down');
             }
         }else{
